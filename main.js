fb9b8614db20873d506098c9c4797aa7f3247b0f
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-05-31 11:32:02 -0600

    pretty code

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 let bertPic = document.querySelector('#bertPic');
 
-bertPic.addEventListener('click', function(){
+bertPic.addEventListener('click', function () {
     window.open('https://youtu.be/io2d_cpoLDg', "_blank");
 });
 
 let bertMarkShane = document.querySelector('#bertMarkShane');
 
-bertMarkShane.addEventListener('click', function(){
+bertMarkShane.addEventListener('click', function () {
     window.open('https://youtu.be/aEXkjbinWT4', "_blank");
 });
 
 let twoDays = document.querySelector('#twoDays');
 
-twoDays.addEventListener('click', function(){
+twoDays.addEventListener('click', function () {
     window.open('https://youtu.be/R0pfZygi2t8', "_blank");
 });
 
 let happyBirthday = document.querySelector('#happyBirthday');
 
-happyBirthday.addEventListener('click', function(){
+happyBirthday.addEventListener('click', function () {
     window.open('https://youtu.be/y2DDhVoxEOw', "_blank");
 });
 
 let wilcoRedRocks = document.querySelector('#wilcoRedRocks');
 
-wilcoRedRocks.addEventListener('click', function(){
+wilcoRedRocks.addEventListener('click', function () {
     window.open('https://youtu.be/WBpDs-BfwO0', "_blank")
 })
